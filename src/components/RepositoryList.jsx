a7d3b28a1f67870c32cd052e67dce7452a2b62f0
monica6423 <monica6423@gmail.com>
2021-01-09 19:03:24 +0200

    organize query

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, View, StyleSheet, Text } from 'react-native';
 import RepositoryItem from './RepositoryItem';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
 const styles = StyleSheet.create({
   separator: {
     height: 10,
@@ -11,25 +14,23 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-    const [repositories, setRepositories] = useState();
+    const { data, error, loading } = useQuery(GET_REPOSITORIES, {fetchPolicy: 'cache-and-network',});
+    // const [repositories, setRepositories] = useState();
 
-    const fetchRepositories = async () => {
-      const response = await fetch('http://192.168.1.105:5000/api/repositories');
-      const json = await response.json();
+    // const fetchRepositories = async () => {
+    //   const response = await fetch('http://192.168.1.105:5000/api/repositories');
+    //   const json = await response.json();
+    //   setRepositories(json);
+    // };
   
-  
-      setRepositories(json);
-    };
-  
-    useEffect(() => {
-      fetchRepositories();
-    }, []);
+    // useEffect(() => {
+    //   fetchRepositories();
+    // }, []);
   
     // Get the nodes from the edges array
-    const repositoryNodes = repositories
-      ? repositories.edges.map(edge => edge.node)
+    const repositoryNodes = data
+      ? data.repositories.edges.map(edge => edge.node)
       : [];
-      console.log('repositoryNodes',repositoryNodes);
 
 
     const renderItem = ({ item }) => (
