12e6b5efd4f82b8e95c709c84defecb6f547b3eb
monica6423 <monica6423@gmail.com>
2021-01-09 10:28:04 +0200

    graphql and apollo client

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
+import { ApolloProvider } from '@apollo/react-hooks';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
-  return <NativeRouter>
-  <Main />
-</NativeRouter>;
+  return (
+  <NativeRouter>
+    <ApolloProvider client={apolloClient}>
+      <Main />
+    </ApolloProvider>
+  </NativeRouter>);
 };
 
 export default App;
